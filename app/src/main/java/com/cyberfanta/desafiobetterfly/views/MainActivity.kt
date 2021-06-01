a982ffff4cfa205aa8d6811a49e1d6294a661314
Julio César León <julioleon2004@gmail.com>
2021-06-01 18:41:55 -0400

    Feat (Author): Adding the author widget
    Adding the view to show the author when select on app menu.

--- app/src/main/java/com/cyberfanta/desafiobetterfly/views/MainActivity.kt
+++ app/src/main/java/com/cyberfanta/desafiobetterfly/views/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.cyberfanta.desafiobetterfly.views
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.graphics.Insets
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowMetrics
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.cyberfanta.desafiobetterfly.R
 import com.google.firebase.analytics.FirebaseAnalytics
 
 
 class MainActivity : AppCompatActivity() {
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
+
+    private var authorOpened: Boolean = false
+    private var deviceWidth: Int = 0
+    private var deviceHeight:Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +31,76 @@ class MainActivity : AppCompatActivity() {
 
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        //Calculate Current Device Size
+        calculateDeviceDimensions()
+
+
+        //Binding onClick function for author menu option
+        val author : ConstraintLayout = findViewById(R.id.author)
+        author.setOnClickListener { authorSelected(author) }
     }
+
+    /**
+     * Process the behavior of the app when user press back button
+     */
+    override fun onBackPressed() {
+        val constraintLayout : ConstraintLayout = findViewById(R.id.author)
+        if (authorOpened) {
+            authorSelected(constraintLayout)
+            authorOpened = false
+            return
+        }
+
+        super.onBackPressed()
+    }
+
+    /**
+     * Show the developer info
+     */
+    private fun authorSelected(view: View) {
+        setAnimation(view, "translationX", 300, false, 0f, deviceWidth.toFloat())
+    }
+
+    /**
+     * Make a simple animation on a view
+     */
+    private fun setAnimation(view: View, propertyName: String, duration: Long, repeat: Boolean, value1: Float, value2: Float) {
+        val objectAnimator = ObjectAnimator.ofFloat(view, propertyName, value1, value2)
+        val animator = AnimatorSet()
+        animator.play(objectAnimator)
+        animator.duration = duration
+        if (repeat) {
+            animator.addListener(
+                object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        super.onAnimationEnd(animation)
+                        animator.start()
+                    }
+                }
+            )
+        }
+        animator.start()
+    }
+
+    /**
+     * Calculate the device dimension
+     */
+    private fun calculateDeviceDimensions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics: WindowMetrics = windowManager.currentWindowMetrics
+            val insets: Insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            deviceWidth = windowMetrics.bounds.width() - insets.left - insets.right
+            deviceHeight = windowMetrics.bounds.height() - insets.top - insets.bottom
+        } else {
+            val displayMetrics = DisplayMetrics()
+            @Suppress("DEPRECATION")
+            windowManager.defaultDisplay.getMetrics(displayMetrics)
+            deviceWidth = displayMetrics.widthPixels
+            deviceHeight = displayMetrics.heightPixels
+        }
+    }
+
+
 }
